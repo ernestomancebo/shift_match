5b169e45b6961e17510e759f20d838fa51a72659
Ernesto Mancebo <ernesmancebo@gmail.com>
2022-11-18 19:53:07 -0400

    uses a builder

--- test/test_models.py
+++ test/test_models.py
@@ -7,16 +7,22 @@ import copy
 class TestModels(unittest.TestCase):
 
     def setUp(self):
+        self.shift_under_test = self.__get_shift()
+
+    def __get_shift() -> Shift:
         clock_in = datetime.today()
         clock_out = datetime.today()
 
         # Add 15 minutes
         clock_out = clock_out + timedelta(minutes=15)
 
-        self.shift_under_test = Shift(clock_in.time(), clock_out.time())
+        shift = Shift(clock_in.time(), clock_out.time())
+        
+        return shift
+
 
     def test_shift_invalid(self):
-        other_shift = copy.copy(self.shift_under_test)
+        other_shift = self.__get_shift()
 
         # Clock in time rebases the clock out
         other_shift.clock_in = (
